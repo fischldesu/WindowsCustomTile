1282dd393facac463cda406eee0f619d0b8b3b46
fischldesu <fischldesu@outlook.com>
2025-06-26 12:20:01 +0800

    Amend some bugs/appearance bug: an unexpected file extension cause Exception at fn: FileOpenPicker.FileTypeFilter.Add

--- LiveTileWinUI3/Utility/FilePicker.cs
+++ LiveTileWinUI3/Utility/FilePicker.cs
@@ -11,22 +11,57 @@ namespace LiveTileWinUI3.Utility
 {
     public class FilePicker
     {
-        public async static Task<string?> ReadText(string[] exts, PickerLocationId pickerLocation = PickerLocationId.DocumentsLibrary)
+        private static FileOpenPicker InitFilePicker()
         {
             var picker = new FileOpenPicker();
-
             InitializeWithWindow.Initialize(picker, WindowNative.GetWindowHandle(App.mainWindow));
+
+            return picker;
+        }
+
+        public async static Task<string?> ReadText(string[] exts, PickerLocationId pickerLocation = PickerLocationId.DocumentsLibrary)
+        {
+            var picker = InitFilePicker();
+
             picker.ViewMode = PickerViewMode.List;
             picker.SuggestedStartLocation = pickerLocation;
 
-            foreach (var ext in exts)
-                picker.FileTypeFilter.Add(ext);
-            
-            var file = await picker.PickSingleFileAsync();
-            if (file != null && file.IsAvailable)
-                return await FileIO.ReadTextAsync(file);
+            try
+            {
+                foreach (var ext in exts)
+                    picker.FileTypeFilter.Add(ext);
+
+                var file = await picker.PickSingleFileAsync();
+                if (file != null && file.IsAvailable)
+                    return await FileIO.ReadTextAsync(file);
+            }
+            catch (Exception) { }
 
             return null;
+        }
+
+        public async static Task<string?> PickSingleFile(string[] exts, bool thumbThumbnailViewMode, PickerLocationId startLocation = PickerLocationId.DocumentsLibrary)
+        {
+            var picker = InitFilePicker();
+
+            if (thumbThumbnailViewMode) picker.ViewMode = PickerViewMode.Thumbnail;
+            else picker.ViewMode = PickerViewMode.List;
+            
+            picker.SuggestedStartLocation = startLocation;
+
+            try
+            {
+                foreach (var ext in exts)
+                    picker.FileTypeFilter.Add(ext);
+
+                var f = await picker.PickSingleFileAsync();
+                return f?.Path;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
         }
     }
 }
