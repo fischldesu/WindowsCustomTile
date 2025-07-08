1b5632ad92cf4afda2688ec98549a1a1f59ee85b
fischldesu <fischldesu@outlook.com>
2025-07-08 22:44:18 +0800

    Add protocol start up Optimize launch logic

--- LiveTileWinUI3/App.xaml.cs
+++ LiveTileWinUI3/App.xaml.cs
@@ -11,13 +11,13 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
-using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using LiveTileWinUI3.Utility.Log;
 using System.Security.Principal;
 using System.Threading.Tasks;
+using Microsoft.Windows.AppLifecycle;
 using Windows.System;
 using LiveTileWinUI3.Utility;
 
@@ -38,16 +38,15 @@ namespace LiveTileWinUI3
         public App()
         {
             this.InitializeComponent();
-
             this.UnhandledException += App_UnhandledException;
         }
 
         private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
         {
             Logger.Log($"App.UnhandledException:{e.Message}");
-            
+
             var logWindow = new LogWindow();
-            
+
             mainWindow?.Close();
             logWindow.Closed += (_, _) => Exit();
 
@@ -60,16 +59,31 @@ namespace LiveTileWinUI3
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            Logger.Log("Application launched （App.OnLaunched)");
+            Logger.Log("Application launched (App.OnLaunched)");
 
             mainWindow = new MainWindow();
             m_window = mainWindow;
 
+            var protocalLaunch = false;
+            if (AppInstance.GetCurrent().GetActivatedEventArgs().Data is ProtocolActivatedEventArgs protocolArgs && protocolArgs.Uri != null)
+                protocalLaunch = ProtocolLaunch(protocolArgs.Uri);
 
-            if (Administrator() || !DoLaunchCommand() || Settings.LaunchAlwaysShowWindow)
+            if (protocalLaunch || !DoLaunchCommand() || Settings.LaunchAlwaysShowWindow)
                 m_window.Activate();
             else
                 Exit();
+        }
+
+        private bool ProtocolLaunch(Uri uri)
+        {
+            Logger.Log($"Application launched with Protocol: {uri}");
+            switch (uri.Host.ToLower())
+            {
+                case "start":
+                    return true;
+                default:
+                    return false;
+            }
         }
 
         public static bool DoLaunchCommand()
@@ -94,13 +108,13 @@ namespace LiveTileWinUI3
                                 ret = true;
                                 break;
                             default:
-                                Logger.Log($"Unknown launch command type:{(commandType is string t ? t: "UnknownType")}", LogMessage.LogLevel.WARNING);
+                                Logger.Log($"Unknown launch command type:{(commandType is string t ? t : "UnknownType")}", LogMessage.LogLevel.WARNING);
                                 ret = false;
                                 break;
                         }
                     }
                 }
-                catch (Exception e) 
+                catch (Exception e)
                 {
                     Logger.Log($"Error while doing Launch Command {e.Message}", LogMessage.LogLevel.ERROR);
                 }
